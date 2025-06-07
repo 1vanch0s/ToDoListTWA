0d8c14bfac6701e1322dfc4b3985b8d2359bb8a2
1vanch0s <1vanch0s133@gmail.com>
2025-06-07 20:09:38 +0300

    /dist fix

--- todo-app/vite.config.ts
+++ todo-app/vite.config.ts
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/ToDoListTWA/', // Префикс для GitHub Pages
+    root: '.', // Указываем текущую папку (todo-app) как корень
+    build: {
+      outDir: '../dist', // Сохраняем собранные файлы в корень /ToDoListTWA/dist
+      assetsDir: 'assets', // Папка для CSS/JS
+    },
+    base: '/', // Корневой путь для Vercel
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_BOT_TOKEN': JSON.stringify(env.VITE_BOT_TOKEN),
+      'import.meta.env.REACT_APP_API_URL': JSON.stringify(env.REACT_APP_API_URL || 'https://todolisttwa.onrender.com'),
     },
   };
 });
